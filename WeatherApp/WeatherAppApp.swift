3dd6268e94e5dd3c3a94d6b1342d5fbcae18eb2b
engy <132174880+GegeSaad@users.noreply.github.com>
2024-08-22 16:19:22 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Engy on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
